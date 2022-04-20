8716b0c84f9cfd3de45c36e19f47295ab9fef715
Matthew <matthewtoro17@gmail.com>
2022-04-20 23:37:00 +0800

    Create functions that detect a win or tie

--- script.js
+++ script.js
@@ -7,11 +7,60 @@ const playerFactory = (name, marker) => {
 const gameBoard = (() => {
   let spaces = Array(9).fill('');
 
+  const _winnerExists = () => {
+    let line;
+    for (let i = 0; i < 8; i++) {
+      switch (i) {
+        case 0:
+          line = spaces[0] + spaces[1] + spaces[2];
+          break;
+        case 1:
+          line = spaces[3] + spaces[4] + spaces[5];
+          break;
+        case 2:
+          line = spaces[6] + spaces[7] + spaces[8];
+          break;
+        case 3:
+          line = spaces[0] + spaces[3] + spaces[6];
+          break;
+        case 4:
+          line = spaces[1] + spaces[4] + spaces[7];
+          break;
+        case 5:
+          line = spaces[2] + spaces[5] + spaces[8];
+          break;
+        case 6:
+          line = spaces[0] + spaces[4] + spaces[8];
+          break;
+        case 7:
+          line = spaces[2] + spaces[4] + spaces[6];
+          break;
+      }
+
+      if (line === 'XXX' || line === 'OOO') return true;
+    }
+
+    return false;
+  };
+
+  const _gameIsTied = () => {
+    return !spaces.includes('');
+  };
+
+  const _checkForGameOver = () => {
+    if (_winnerExists()) {
+      console.log('winner detected');
+    } else if (_gameIsTied()) {
+      console.log('tie');
+    }
+  };
+
   const placeMarkerAtIndex = (index => {
     if (spaces[index] === '') {
       gameBoard.spaces[index] = gameController.currentPlayer.marker;
       gameController.changeCurrentPlayer();
       displayController.displayGameBoard();
+      _checkForGameOver();
     }
   });
 
